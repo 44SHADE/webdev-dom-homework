20708e75bf8149686845e36e0aa3adfb531d4305
44SHADE <xorolich@mail.ru>
2025-05-04 18:41:27 +0300

    small changes

---
+++ utils/commentsRenderer.js
@@ -0,0 +1,42 @@
+export function renderer(commentsData) {
+  const container = document.querySelector('.comments');
+  if (commentsData.length === 0) return [];
+
+  container.innerHTML = commentsData
+    .map(
+      (el, index) =>
+        ` <li class="comment" data-index=${index}>
+        <div class="comment-header">
+          <div>${el.name}</div>
+          <div>${el.date}</div>
+        </div>
+        <div class="comment-body">
+          <div class="comment-text">
+            ${el.text}
+          </div>
+        </div>
+        <div class="comment-footer">
+          <div class="likes">
+            <span class="likes-counter">${el.count}</span>
+            <button class="like-button ${
+              el.isLiked ? '-active-like' : null
+            }"></button>
+          </div>
+        </div>
+      </li>
+      `,
+    )
+    .join('');
+
+  const comments = document.querySelectorAll('.comment');
+  comments.forEach((el, index) => {
+    const commentData = commentsData[index];
+    const btn = el.querySelector('.like-button');
+    btn.addEventListener('click', (evt) => {
+      evt.stopPropagation();
+      commentData.count += commentData.isLiked ? -1 : 1;
+      commentData.isLiked = !commentData.isLiked;
+      renderer(commentsData);
+    });
+  });
+}
---
+++ utils/xssValidate.js
@@ -0,0 +1,8 @@
+export default function xssValidate(string) {
+  return string
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/'/g, '&#x27;')
+    .replace(/"/g, '&quot;');
+}
